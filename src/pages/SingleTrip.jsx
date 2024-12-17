f0b0ea781f8f5d65cd7c605eaf39a417d26cf20f
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-12-17 16:53:30 +0100

    add style to single trip cards and add navlink to homepage

--- src/pages/SingleTrip.jsx
+++ src/pages/SingleTrip.jsx
@@ -1,16 +1,34 @@
-
+import visitatori from "../database/visitatori"
+import { useNavigate } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 export default function SingleTrip() {
+
+    const navigate = useNavigate()
 
     return (
 
         <>
-            <button className="btn btn-primary">Torna ai Viaggi</button>
-            <div className="card">
-                <div className="card-body">
-                    <div>Nome Viaggiatore</div>
+            <div className="d-flex justify-content-center mt-5 mb-5">
+                <NavLink to='/' button className="btn btn-primary">Torna ai Viaggi</NavLink>
+            </div>
+
+            <div className="container ">
+                <div className="row row-cols-4 flex-wrap  gap-3 justify-content-center">
+                    {visitatori.map(visitatore => (
+                        <div className="card col g-2" key={visitatore.id}>
+                            <h4 className="text-center">{visitatore.nome} {visitatore.cognome}</h4>
+                            <div className="d-flex justify-content-center">
+                                <button onClick={() => (navigate(visitatori.id))} className="btn btn-primary btn-sm mb-3"> Altre info</button>
+                            </div>
+                        </div>
+
+
+                    ))}
+
                 </div>
             </div>
+
         </>
 
     )
