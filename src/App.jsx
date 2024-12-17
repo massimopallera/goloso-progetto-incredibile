1de799b290575aba74b4c4553ad6c0ae6490202a
Gabbocnc <cianci.gabriele@hotmail.it>
2024-12-17 15:44:45 +0100

    edit forlder

--- src/App.jsx
+++ src/App.jsx
@@ -49,54 +49,6 @@ function App() {
               </div>
             </div>
 
-
-            <div className="card-lg text-left ">
-              <div className="card-body rounded-4 ">
-                <div><strong>Dubai</strong></div>
-                <div>YYYY/MM/DD</div>
-                <button
-                  className='btn btn-primary btn-sm bg-primary rounded-4 text-white'>
-                  <strong>Show Travel</strong>
-                </button>
-              </div>
-            </div>
-
-
-            <div className="card-lg text-left ">
-              <div className="card-body rounded-4 ">
-                <div><strong>Dubai</strong></div>
-                <div>YYYY/MM/DD</div>
-                <button
-                  className='btn btn-primary btn-sm bg-primary rounded-4 text-white'>
-                  <strong>Show Travel</strong>
-                </button>
-              </div>
-            </div>
-
-
-            <div className="card-lg text-left ">
-              <div className="card-body rounded-4 ">
-                <div><strong>Dubai</strong></div>
-                <div>YYYY/MM/DD</div>
-                <button
-                  className='btn btn-primary btn-sm bg-primary rounded-4 text-white'>
-                  <strong>Show Travel</strong>
-                </button>
-              </div>
-            </div>
-
-
-            <div className="card-lg text-left ">
-              <div className="card-body rounded-4 ">
-                <div><strong>Dubai</strong></div>
-                <div>YYYY/MM/DD</div>
-                <button
-                  className='btn btn-primary btn-sm bg-primary rounded-4 text-white'>
-                  <strong>Show Travel</strong>
-                </button>
-              </div>
-            </div>
-
           </div>
 
         </div>
