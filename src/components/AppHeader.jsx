458dd9232f7cecec0191acb21fcb0c227eb33f51
Gabbocnc <cianci.gabriele@hotmail.it>
2024-12-17 22:35:02 +0100

    add css for navLink Header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,11 +3,15 @@ import { NavLink } from "react-router-dom";
 export default function AppHeader() {
     return (
         <header className="bg-dark">
-            <div className='text-white d-flex gap-3 p-2'>
-                {/* <div>
-                    <i className="bi bi-layout-sidebar"></i>
-                </div> */}
-                <NavLink className="nav-link p-1" aria-disabled="false" to="/">Home</NavLink>
+
+            <div className=' container text-white d-flex gap-3 p-2 align-items-center '>
+                <h1>BoolTrips</h1>
+                <NavLink
+                    className="nav-link p-3"
+                    aria-disabled="false"
+                    to="/">
+                    Home
+                </NavLink>
             </div>
         </header>
     )
