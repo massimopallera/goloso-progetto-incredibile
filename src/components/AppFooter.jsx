1cb492e26019a11b05bd8bbcd7fd29495da9ba93
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-12-17 17:00:50 +0100

    "add style footer"

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,7 +1,11 @@
 export default function AppFooter() {
     return (
-        <footer>
-            Footer
+        <footer className="bg-dark text-light d-flex align-items-center justify-content-center" style={{ minHeight: "80px" }}>
+            <div style={{ fontSize: "0.75rem" }}>
+                <p>
+                    © 2024 Boolean Trips - All rights reserved.
+                </p>
+            </div>
         </footer>
     )
 }
