b044a7753f4b962f482419cd567342a532b29133
massimopallera <pallera.massimo@gmail.com>
2024-12-17 16:37:45 +0100

    added logic to get Partecipant

--- src/pages/SinglePartecipant.jsx
+++ src/pages/SinglePartecipant.jsx
@@ -1,5 +1,29 @@
+import { useParams } from 'react-router-dom'
+import visitatori from '../database/visitatori.js'
+
+// Dare parametri alle rotte --> in questo caso nella rotta dovra` esserci l'id del partecipante
+// import { useParams } from 'react-router-dom'
+
+
 export default function SinglePartecipant() {
+
+    // 📌 prendere id del partecipante
+    // const {id} = useParams()
+
+
+    //confrontare id del partecipante all'interno di visitatore
+    function getPartecipant(id){
+        const tripClient = visitatori.find(element => element.id === id);
+        return tripClient
+    }
+
+
+    console.log(getPartecipant("V001_1"));
+    
+
     return (
-        <></>
+        <>
+
+        </>
     )
 }
