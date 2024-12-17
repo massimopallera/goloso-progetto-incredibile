7b097a7e0048cf0061995f27bb2e619b8afb5b8e
Gabbocnc <cianci.gabriele@hotmail.it>
2024-12-17 16:23:47 +0100

    edit cardTour

--- src/components/CardTour.jsx
+++ src/components/CardTour.jsx
@@ -1,18 +1,21 @@
+import viaggi from "../database/viaggi"
+
 export default function CardTour() {
     return (
         <div className="container table d-flex flex-column gap-2 ">
-
-            <div className="card-lg text-left ">
-                <div className="card-body rounded-4 ">
-                    <div><strong>Dubai</strong></div>
-                    <div>YYYY/MM/DD</div>
-                    <button
-                        className='btn btn-primary btn-sm bg-primary rounded-4 text-white'>
-                        <strong>Show Travel</strong>
-                    </button>
+            {viaggi.map((viaggio) =>
+                <div key={viaggio.id} className="card-lg text-left ">
+                    <div className="card-body rounded-4 ">
+                        <div><strong>{viaggio.nome}</strong></div>
+                        <div>{viaggio.dataPartenza}</div>
+                        <div>{viaggio.dataRitorno}</div>
+                        <button
+                            className='btn btn-primary btn-sm bg-primary rounded-4 text-white'>
+                            <strong>Show Travel</strong>
+                        </button>
+                    </div>
                 </div>
-            </div>
-
+            )}
         </div>
     )
 }
