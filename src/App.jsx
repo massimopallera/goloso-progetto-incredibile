17ab23ded90b603a9891575f0faf226c8ae6ca7a
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-12-17 15:55:05 +0100

    add browserRouter and AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import AppLayout from './layouts/AppLayout'
+import HomePage from './pages/HomePage'
 
 
 function App() {
@@ -7,57 +9,15 @@ function App() {
 
   return (
     <>
-      {/* HEADER */}
-      <header>
-        <div className='bg-dark text-white d-flex gap-3 p-2 justify-content-around'>
-          <div>
-            <i className="bi bi-layout-sidebar"></i>
-          </div>
-          <a className="nav-link" aria-disabled="false" href="#">Home</a>
-          <a className="nav-link" aria-disabled="false" href="#">Viaggi</a>
-          <a className="nav-link" aria-disabled="false" href="#">Partecipanti</a>
-        </div>
-      </header>
+      <BrowserRouter>
+        <Routes>
 
-      {/* MAIN */}
-      <main className='container-lg mt-3 '>
-        <div className='bg-dark p-2 d-flex flex-column gap-2 rounded-4 table p-3'>
+          <Route element={<AppLayout />}>
+            <Route index element={<HomePage />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
 
-          {/* search bar */}
-          <div className="searchBar">
-            <form className="d-flex mb-3" role="search">
-              <input
-                className="form-control rounded-4"
-                type="search"
-                placeholder="Cerca Il Viaggio"
-                aria-label="Search"
-              />
-            </form>
-          </div>
-
-          {/* card per i viaggi */}
-          <div className="container table d-flex flex-column gap-2 ">
-
-            <div className="card-lg text-left ">
-              <div className="card-body rounded-4 ">
-                <div><strong>Dubai</strong></div>
-                <div>YYYY/MM/DD</div>
-                <button
-                  className='btn btn-primary btn-sm bg-primary rounded-4 text-white'>
-                  <strong>Show Travel</strong>
-                </button>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-      </main>
-
-      {/* FOOTER */}
-      <footer>
-        FOOTER
-      </footer>
     </>
   )
 }
