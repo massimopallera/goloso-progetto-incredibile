cb07145bc9dd0a2388c1c2b38061fa3721301f0f
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-12-17 16:04:20 +0100

    rmove singletrip in pages folder and delete a file

--- src/components/SingleTrip.jsx
+++
@@ -1,8 +0,0 @@
-
-
-export default function SingleTrip() {
-
-    return (
-
-    )
-}
---
+++ src/pages/SingleTrip.jsx
@@ -0,0 +1,17 @@
+
+
+export default function SingleTrip() {
+
+    return (
+
+        <>
+            <button className="btn btn-primary">Torna ai Viaggi</button>
+            <div className="card">
+                <div className="card-body">
+                    <div>Nome Viaggiatore</div>
+                </div>
+            </div>
+        </>
+
+    )
+}
